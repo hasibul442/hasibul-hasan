b33d4abd55a8db1435b196bc35b800989de63550
Md. Hasibul Hasan <hasibulhasan442@gmail.com>
2020-07-30 23:46:05 +0600

    update

--- site/js/circle.js
+++ site/js/circle.js
@@ -1,5 +1,5 @@
 function Circlle(el){
-    $(el).circleProgress({fill: {color: '#f25c54'}})
+    $(el).circleProgress({fill: {color: '#9bf6ff'}})
     .on('circle-animation-progress',function(event,progress,stepValue){
         $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%');
     });
